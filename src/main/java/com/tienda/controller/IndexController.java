c9cb3f55e0d2152482204da0d6e81c71a887e517
scv13 <scv13@192.168.0.102>
2022-02-25 11:24:20 -0600

    Desarrollo de la semana#5

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -8,22 +8,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.tienda.service.ClienteService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
 public class IndexController {
+
     @Autowired
-    private ClienteDao clienteDao;
+    private ClienteService clienteService;
 
     @GetMapping("/")
     public String inicio(Model model) {
         log.info("Se muestra la pagina index de templates");
-        
-        
-        
-        var clientes = clienteDao.findAll();
-        
+        var clientes = clienteService.getClientes();
         model.addAttribute("clientes", clientes);
         return "index";
+    }
+
+    @GetMapping("/nuevoCliente")
+    public String nuevoCliente(Cliente cliente) {
+
+        return "modificarCliente";
+    }
+
+    @PostMapping("/guardarCliente")
+    public String guardarCliente(Cliente cliente) {
+        clienteService.save(cliente);
+        return "redirect:/";
+    }
+
+    @GetMapping("/modificarCliente/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model) {
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente",cliente);
+        return "modificarCliente";
+    }
+    
+    @GetMapping("/eliminarCliente/{idCliente}")
+    public String eliminarCliente(Cliente cliente) {
+        clienteService.delete(cliente);
+        return "redirect:/";
     }
 }
